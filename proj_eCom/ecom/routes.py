82210c8e3d6469d8a4d5fd5bd9ae391c35581513
mohdrab01 <63246629+mohdrab01@users.noreply.github.com>
2020-05-01 11:18:35 +0530

    Update routes.py

--- proj_eCom/ecom/routes.py
+++ proj_eCom/ecom/routes.py
@@ -26,6 +26,7 @@ def home():
 @app.route('/signup',methods=['GET','POST'])
 def signup():
 	if current_user.is_authenticated:
+		flash("First logout inorder to signup!",'primary')
 		return redirect(url_for('index'))
 	if request.method=='POST':
 		user=User(username=request.form['username'],email=request.form['email'],password=request.form['password'])
@@ -46,21 +47,14 @@ def login():
 		user = User.query.filter_by(email=email).first()
 		if user and user.password==password:
 			login_user(user)
-			flash('You are logged in')
+			flash('You are logged in','success')
 			return redirect(url_for('index'))
 		else:
 			error = 'Invalid username or password. Please try again!'
+			flash('Invalid email or Password!','danger')
 		return redirect(url_for('home'))
 	return render_template('login.html',title='login',error=error)
 
-
-# @app.route("/user")
-# def user():
-# 	if "user" in session:
-# 		user=session["user"]
-# 		return f"<h1>{user}</h1>"
-# 	else:
-# 		return redirect(url_for("login"))
 
 ##### INDEX PAGE #####
 @login_required
@@ -92,8 +86,8 @@ def cart():
 			product.append(p)
 		return render_template('cart.html',product=product)
 	else:
-		flash('Your cart is Empty ')
-		return redirect(url_for('cart'))
+		flash('Your cart is Empty ','warning')
+		return redirect(url_for('index'))
 
 
 ##### ADD GADGET TO HOME AND INDEX #####
@@ -108,14 +102,14 @@ def add_to_cart():
 		quantity=15
 		userid=current_user.id
 		cart=Cart(id=userid,productid=productid,quantity=quantity)
-		cart1=Cart.query.filter_by(id=userid,productid=productid)
+		cart1=Cart.query.filter_by(id=userid,productid=productid).first()
 		if cart1 :
-			flash('Item already in cart!')
+			flash('Item already in cart!','warning')
 			return redirect(url_for('index'))	
 		else:
 			db.session.add(cart)
 			db.session.commit()
-			flash('Item added to cart succesfully!')
+			flash('Item added to cart succesfully!','success')
 			return redirect(url_for('index'))
 	# return render_template('cart.html',title='cart')
 
@@ -123,21 +117,27 @@ def add_to_cart():
 @login_required
 @app.route('/place_order',methods=['GET','POST'])
 def place_order():
-	if request.method=='POST':
-		productid=request.form['productid']
-		quantity=1
-		userid=current_user.id
-		order=Order(id=userid,productid=productid,quantity=quantity)
-		db.session.add(order)
-		db.session.commit()
-		flash('Placed order for item succesfully!')
-		# cart=Cart.query.filter_by(id=userid,productid=productid,quantity=quantity)
-		# db.session.delete(cart)
-	#change made #	db.session.flush()#
-		# db.session.commit()
-		# flash('Item removed from your cart!')
-		return redirect(url_for('index'))
-	# return render_template('cart.html',title='cart')
+	try:
+		if request.method=='POST':
+			productid=request.form['productid']
+			quantity=1
+			userid=current_user.id
+			order=Order(id=userid,productid=productid,quantity=quantity)
+			db.session.add(order)
+			# raise ValueError("Some Inegrity Constraint Error Happened")
+			# db.session.add(order)
+			db.session.commit()
+			flash('Placed order for item succesfully!','success')
+			cart=Cart.query.filter_by(id=userid,productid=productid,quantity=quantity)
+			# db.session.delete(cart)
+			cart.delete()
+			db.session.commit()
+			flash('Item removed from your cart!','success')
+			return redirect(url_for('index'))
+		# return render_template('cart.html',title='cart')
+	except :
+		flash('Cannot place order, as Item already is in cart!','warning')
+		return redirect(url_for('cart'))
 
 
 ##### MY ORDERS PAGE #####
@@ -153,12 +153,29 @@ def orders():
 			product.append(p)
 		return render_template('orders.html',product=product,order=order,title='orders')
 	else:
-		flash('You have no orders yet! ')
-		return redirect(url_for('order'))
+		flash('You have no orders yet! ','warning')
+		return redirect(url_for('index'))
 
 ##### PAYMENT PAGE #####
 @login_required
 @app.route('/payment',methods=['GET','POST'])
 def payment():
-	flash('Make your payment!')
+	flash('Make your payment!','primary')
 	return render_template('payment.html',title='payment')
+
+
+##### FEEDBACK PAGE #####
+@login_required
+@app.route('/feedback',methods=['GET','POST'])
+def feedback():
+	return render_template('feedback.html',title='feedback')
+
+'''TEST ROUTES'''
+##### description PAGE #####
+@login_required
+@app.route("/details/<string:pid>",methods=['GET','POST'])
+def details(pid):
+	product=Product.query.filter_by(productid=pid).first()
+	return render_template('details.html',title='details',products=product)
+
+
